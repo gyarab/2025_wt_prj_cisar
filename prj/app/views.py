182d619d24d1f87341845139a978a751a45251ba
CisarVaclav <vaclav.cisar.@gyarab.cz git config --global user.email>
2026-02-20 09:01:23 +0100

    save

--- prj/app/views.py
+++ prj/app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def render_home(request):
-    return render(request, 'home.html')
+    return render(request, 'app/home.html')
 
 def render_about(request):
     return render(request, 'about.html')
