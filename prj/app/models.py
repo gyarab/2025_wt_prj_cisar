5632ca59ef567a6751caff4ffca563006c638241
CisarVaclav <vaclav.cisar.@gyarab.cz git config --global user.email>
2026-03-20 11:52:41 +0100

    save

--- prj/app/models.py
+++ prj/app/models.py
@@ -18,3 +18,13 @@ class Player(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Match(models.Model):
+    home_team = models.ForeignKey(Team, related_name='home_matches', on_delete=models.CASCADE)
+    away_team = models.ForeignKey(Team, related_name='away_matches', on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    stadium = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.home_team.name} vs {self.away_team.name} on {self.date}"
+    
