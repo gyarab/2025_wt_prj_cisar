aaf9a7912f925b8b01087ba9b0e36d9eacf0e8a6
CisarVaclav <vaclav.cisar.@gyarab.cz git config --global user.email>
2026-03-20 12:02:51 +0100

    save

--- prj/app/admin.py
+++ prj/app/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 from .models import Team, Player, Match
 
-admin.site.register(Team)
-admin.site.register(Player)
-admin.site.register(Match)
 
 @admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
